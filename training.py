bd1c581ef9126b6e2f554c71f3c297a68d1018b3
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-02 01:19:21 +0900

    using the intents for the training file

--- training.py
+++ training.py
@@ -14,3 +14,22 @@ model = tf.keras.Sequential([
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+
+lemmatizer = WordNetLemmatizer
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.tokenize(pattern)
+        words.append(word_list)
+        documents.append((word_list), intent['tag'])
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+print(documents)
