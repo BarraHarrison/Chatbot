3d806e2d998ec3eaf73c4fc0a0799216329d73ce
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-02 01:11:30 +0900

    importing tensorflow as tf

--- training.py
+++ training.py
@@ -4,3 +4,13 @@ import json
 import pickle
 import numpy as np
 import nltk
+from nltk.stem import WordNetLemmatizer
+import tensorflow as tf
+
+# Example of creating a model using tf.keras
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
