1ac196eeb1c5a7d6c329e718360ee3a41a7cfbdf
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-03 02:22:02 +0900

    Training the neural network

--- training.py
+++ training.py
@@ -62,3 +62,7 @@ training = np.array(training)
 
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.model')
+print("Done!")
