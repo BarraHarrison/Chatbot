34d9a13b4beb11eda079d54f82dc97e2711c537d
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-03 02:42:51 +0900

    creating the functions

--- main.py
+++ main.py
@@ -6,3 +6,26 @@ import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
 import tensorflow as tf
+
+# Load intents file
+lemmatizer = WordNetLemmatizer()
+with open('intents.json') as file:
+    intents = json.load(file)
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.model')
+
+# Functions
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
