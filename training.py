ee93cb92ef702cc0c2d62dd9faa1b967ab64576e
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-03 02:27:28 +0900

    debugging to train and save the model

--- training.py
+++ training.py
@@ -7,18 +7,16 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 import tensorflow as tf
 
-# Example of creating a model using tf.keras
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
+# Ensure NLTK resources are downloaded
+nltk.download('punkt')
+nltk.download('wordnet')
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-
+# Load intents file
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('intents.json').read())
+with open('intents.json') as file:
+    intents = json.load(file)
 
+# Preprocess data
 words = []
 classes = []
 documents = []
@@ -32,17 +30,15 @@ for intent in intents['intents']:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignore_letters]
 words = sorted(set(words))
-
 classes = sorted(set(classes))
 
+# Save words and classes
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
-
-# The neural network needs numbers
-# This is the machine learning part
+# Create training data
 training = []
 output_empty = [0] * len(classes)
 
@@ -51,18 +47,33 @@ for document in documents:
     word_patterns = document[0]
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for word in words:
-        bag.append(1) if word in word_patterns else bag.append(0)
+        bag.append(1 if word in word_patterns else 0)
     
     output_row = list(output_empty)
     output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
 
 random.shuffle(training)
-training = np.array(training)
+training = np.array(training, dtype=object)
 
-train_x = list(training[:, 0])
-train_y = list(training[:, 1])
+# Split training data
+train_x = np.array(list(training[:, 0]))
+train_y = np.array(list(training[:, 1]))
 
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_model.model')
-print("Done!")
+# Build the model
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(128, input_shape=(len(train_x[0]),), activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(len(classes), activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+
+# Save the model
+model.save('chatbot_model.h5')
+print("Model training complete!")
